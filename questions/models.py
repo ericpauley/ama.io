330318e2941577f91be2a24fb63acf5141752e52
zonedabone <zonedabone@gmail.com>
2013-05-18 04:06:02 -0400

    Use actual objects for ForeignKeys.

--- questions/models.py
+++ questions/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class AMASession(models.Model):
     '''
     Question answering sessions are represented by this model.
     '''
-    owner = models.ForeignKey('User', related_name='sessions', editable=False)
+    owner = models.ForeignKey(User, related_name='sessions', editable=False)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     
@@ -16,11 +17,11 @@ class AMAQuestion(models.Model):
     Questions asked by users are represented by this model.
     '''
     
-    asker = models.ForeignKey('User', related_name='own_questions', editable=False)
-    target = models.ForeignKey('User', related_name='asked_questions', editable=False)
+    asker = models.ForeignKey(User, related_name='own_questions', editable=False)
+    target = models.ForeignKey(User, related_name='asked_questions', editable=False)
     question = models.TextField()
     
-    session = models.ForeignKey('AMASession', related_name='questions', editable=False, blank=True, null=True)
+    session = models.ForeignKey(AMASession, related_name='questions', editable=False, blank=True, null=True)
     
     created = models.DateTimeField(auto_now_add=True, editable=False)
     edited = models.DateTimeField(auto_now=True, editable=False)
@@ -31,7 +32,7 @@ class AMAAnswer(models.Model):
     Answers to questions are represented by this model.
     '''
     
-    question = models.OneToOneField('AMAQuestion', related_name='answer', editable=False)
+    question = models.OneToOneField(AMAQuestion, related_name='answer', editable=False)
     response = models.TextField()
     
     created = models.DateTimeField(auto_now_add=True, editable=False)
@@ -42,8 +43,8 @@ class AMAVote(models.Model):
     Users' votes on questions are represented here.
     '''
     
-    user = models.ForeignKey('User', related_name='votes', editable=False)
-    question = models.ForeignKey('AMAQuestion', related_name='votes', editable=False)
+    user = models.ForeignKey(User, related_name='votes', editable=False)
+    question = models.ForeignKey(AMAQuestion, related_name='votes', editable=False)
     
     VOTE_CHOICES = (
         (1, 'Upvote'),
