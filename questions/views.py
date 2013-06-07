56ac4cda365e8bffb1ea70c1d91cdc118383188e
zonedabone <zonedabone@gmail.com>
2013-06-06 23:20:30 -0400

    Rename body.py to base.py

--- questions/views.py
+++ questions/views.py
@@ -2,10 +2,10 @@
 from django.http import Http404
 from django.shortcuts import render
 
-from questions.models import Session
+from questions.models import AMASession
 
 def test(request):
-    return render(request, "body.html")
+    return render(request, "base.html")
     
 def session(request, slug):
     try:
