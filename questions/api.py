001099202409da70e5f71b3a5722fd13da49368c
zonedabone <zonedabone@gmail.com>
2013-06-12 23:35:31 -0400

    Add login API

--- questions/api.py
+++ questions/api.py
@@ -5,6 +5,10 @@ from questions.models import AMASession,AMAQuestion,AMAAnswer
 from questions.authorization import SessionAuthorization
 from django.contrib.auth.models import User
 from django.db.models import Sum
+from django.contrib.auth import authenticate, login, logout
+from tastypie.http import HttpUnauthorized, HttpForbidden
+from django.conf.urls import url
+from tastypie.utils import trailing_slash
 
 class UserResource(ModelResource):
     class Meta:
@@ -13,6 +17,56 @@ class UserResource(ModelResource):
         fields = ['username', 'first_name', 'last_name', 'last_login']
         allowed_methods = ['get']
         authorization = ReadOnlyAuthorization()
+        filtering = {
+            'first_name': ALL,
+            'last_name': ALL,
+            'username': ALL
+        }
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/login%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('login'), name="api_login"),
+            url(r'^(?P<resource_name>%s)/logout%s$' %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('logout'), name='api_logout'),
+        ]
+
+    def login(self, request, **kwargs):
+        print request.META.get('CONTENT_TYPE', 'application/json')
+        self.method_check(request, allowed=['post'])
+
+        data = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+
+        username = data.get('username', '')
+        password = data.get('password', '')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return self.create_response(request, {
+                    'success': True
+                })
+            else:
+                return self.create_response(request, {
+                    'success': False,
+                    'reason': 'disabled',
+                    }, HttpForbidden )
+        else:
+            return self.create_response(request, {
+                'success': False,
+                'reason': 'incorrect',
+                }, HttpUnauthorized )
+
+    def logout(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        if request.user and request.user.is_authenticated():
+            logout(request)
+            return self.create_response(request, { 'success': True })
+        else:
+            return self.create_response(request, { 'success': False }, HttpUnauthorized)
 
 class SessionResource(ModelResource):
     owner = fields.ForeignKey(UserResource, 'owner')
@@ -22,6 +76,11 @@ class SessionResource(ModelResource):
         resource_name = 'session'
         fields = ['owner', 'start_time', 'end_time']
         authorization = SessionAuthorization()
+        filtering = {
+            'owner': ALL_WITH_RELATIONS,
+            'start_time': ALL,
+            'end_time': ALL
+        }
 
 class QuestionResource(ModelResource):
 
