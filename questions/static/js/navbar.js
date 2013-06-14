e1aa1701746b1652842dd9a71e3de9b4d63683cb
zonedabone <zonedabone@gmail.com>
2013-06-14 01:24:53 -0400

    Add registration form checking
    Next up, submitting!

--- questions/static/js/navbar.js
+++ questions/static/js/navbar.js
@@ -10,3 +10,19 @@ $("#login_form").submit(function(event) {
 		$("#login").addClass("btn-danger")
 	})
 })
+
+$("#reg_confirm").change(function() {
+	if($("#reg_confirm").val() === $("#reg_password").val()){
+		$("#reg_confirm_group").removeClass("error")
+	}else{
+		$("#reg_confirm_group").addClass("error")
+	}
+})
+
+$("#reg_terms").click(function(){
+	if($("#reg_terms").prop('checked')){
+		$("#reg_submit").prop("disabled", false)
+	}else{
+		$("#reg_submit").prop("disabled", true)
+	}
+})
