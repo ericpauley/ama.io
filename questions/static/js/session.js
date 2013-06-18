2705669f60ba95ae5467928e4d68d80840cc575b
Eric Pauley <zonedabone@gmail.com>
2013-06-18 02:06:52 -0400

    Lock votes from changing while submitting.

--- questions/static/js/session.js
+++ questions/static/js/session.js
@@ -1,14 +1,17 @@
 $(".upvote").click(function(){
 	if(GLOBALS.auth){
 		var id = $(this).attr("data-question")
+		$("#question-"+id).addClass("lock")
 		if($(this).hasClass("btn-success")){
 			$(this).removeClass("btn-success");
 			$.post("/api/v1/question/"+id+"/vote/",
 				{'vote': 0},
 				function(data){
 					$("#score-"+id).text(data['score'])
-				}
-				);
+				})
+				setTimeout(function(){
+					$("#question-"+id).removeClass("lock")
+				}, 500)
 		}else{
 			$(this).addClass("btn-success");
 			$("#downvote-"+id).removeClass("btn-danger")
@@ -16,8 +19,10 @@ $(".upvote").click(function(){
 				{'vote': 1},
 				function(data){
 					$("#score-"+id).text(data['score'])
-				}
-				);
+				})
+				setTimeout(function(){
+					$("#question-"+id).removeClass("lock")
+				}, 500)
 		}
 	}else{
 		$("#loginModal").modal()
@@ -27,14 +32,17 @@ $(".upvote").click(function(){
 $(".downvote").click(function(){
 	if(GLOBALS.auth){
 		var id = $(this).attr("data-question")
+		$("#question-"+id).addClass("lock")
 		if($(this).hasClass("btn-danger")){
 			$(this).removeClass("btn-danger");
 			$.post("/api/v1/question/"+id+"/vote/",
 				{'vote': 0},
 				function(data){
 					$("#score-"+id).text(data['score'])
-				}
-				);
+				})
+				setTimeout(function(){
+					$("#question-"+id).removeClass("lock")
+				}, 500)
 		}else{
 			$(this).addClass("btn-danger");
 			$("#upvote-"+id).removeClass("btn-success")
@@ -42,12 +50,22 @@ $(".downvote").click(function(){
 				{'vote': -1},
 				function(data){
 					$("#score-"+id).text(data['score'])
-				}
-				);
+				})
+				setTimeout(function(){
+					$("#question-"+id).removeClass("lock")
+				}, 500)
 		}
 	}else{
 		$("#loginModal").modal()
 	}
+})
+
+$("#ask-submit").click(function(){
+	$.post("/api/v1/session/"+GLOBALS['session']+"/ask/",
+		{'question': $("#ask-question").val()},
+		function(){
+			$("#askModal").modal("hide")
+		})
 })
 
 $(function(){
@@ -70,23 +88,29 @@ $(function(){
 						return
 					}
 					if(question['answer'] == null){
-						$("#unansweredlist").append($("#question-"+id))
+						if($("#unansweredlist").has("#question-"+id).length == 0){
+							$("#unansweredlist").append($("#question-"+id))
+						}
 						$("#answer-"+id).hide()
 					}else{
-						$("#answeredlist").append($("#question-"+id))
+						if($("#answeredlist").has("#question-"+id).length == 0){
+							$("#answeredlist").append($("#question-"+id))
+						}
 						$("#answer-"+id).show()
 						$("#answer-text-"+id).text(question['answer']['response'])
 					}
-					$("#score-"+id).text(question['score'])
-					if(question['vote'] == 1){
-						$("#upvote-"+id).addClass("btn-success")
-					}else{
-						$("#upvote-"+id).removeClass("btn-success")
-					}
-					if(question['vote'] == -1){
-						$("#downvote-"+id).addClass("btn-danger")
-					}else{
-						$("#downvote-"+id).removeClass("btn-danger")
+					if(!$("#question-"+id).hasClass("lock")){
+						$("#score-"+id).text(question['score'])
+						if(question['vote'] == 1){
+							$("#upvote-"+id).addClass("btn-success")
+						}else{
+							$("#upvote-"+id).removeClass("btn-success")
+						}
+						if(question['vote'] == -1){
+							$("#downvote-"+id).addClass("btn-danger")
+						}else{
+							$("#downvote-"+id).removeClass("btn-danger")
+						}
 					}
 				}
 				$(".question").each(function(){
