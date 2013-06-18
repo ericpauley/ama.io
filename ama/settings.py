f66d6585e1342962954aa94d02541e6dde0497d5
Eric Pauley <zonedabone@gmail.com>
2013-06-17 22:52:52 -0400

    Move bootstrap to vendor dir.
    This should fix github tracking.

--- ama/settings.py
+++ ama/settings.py
@@ -73,6 +73,7 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(__file__), 'static'),
+    os.path.join(PROJECT_PATH, 'vendor/static'),
 )
 
 # List of finder classes that know how to find static files in
