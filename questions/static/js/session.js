bd88b77f8ac9081ec19dc8bb19d698fd1d6a7f43
zonedabone <zonedabone@gmail.com>
2013-08-20 06:53:52 -0400

    New disqus embed.
    SO MANY IFRAMES

--- questions/static/js/session.js
+++ questions/static/js/session.js
@@ -175,38 +175,3 @@ function check(){
 $(function(){
 	setInterval(check, 1000)
 })
-
-//global vars used by disqus
-var disqus_developer = 1;
-var disqus_shortname = 'amasite';
-var disqus_identifier; //made of post id &nbsp; guid
-var disqus_url; //post permalink
-
-function loadDisqus(source, identifier, url) {
-
-   if (window.DISQUS) {
-   $('#disqus_thread').appendTo(source.parent()); //append the HTML to the control parent
-
-   //if Disqus exists, call it's reset method with new parameters
-   DISQUS.reset({
-      reload: true,
-      config: function () {
-      this.page.identifier = identifier;
-      this.page.url = url;
-      }
-   });
-
-} else {
-
-  //insert a wrapper in HTML after the relevant "show comments" link
-  $('<div id="disqus_thread"></div>').insertAfter(source);
-  disqus_identifier = identifier; //set the identifier argument
-  disqus_url = url; //set the permalink argument
-
-  //append the Disqus embed script to HTML
-  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-  dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-  $('head').append(dsq);
-
-}
-};
