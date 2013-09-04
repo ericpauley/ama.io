3b7fb011aac7e3795b2068f23e2b8ac1508932dd
zonedabone <zonedabone@gmail.com>
2013-09-03 22:46:37 -0400

    Image and SES changes.

--- ama/settings_live.py
+++ ama/settings_live.py
@@ -15,9 +15,13 @@ DATABASES = {
 INSTALLED_APPS += ('storages',)
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+EMAIL_BACKEND = 'django_ses.SESBackend'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
 AWS_STORAGE_BUCKET_NAME = "ama.io"
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_SECURE_URLS = False
+
+AWS_SES_REGION_NAME = 'us-east-1'
+AWS_SES_REGION_ENDPOINT = 'email.us-east-1.amazonaws.com'
