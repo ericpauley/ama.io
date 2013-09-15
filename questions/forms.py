1aed1669b90472f7032d67189c17aa6b66820470
zonedabone <zonedabone@gmail.com>
2013-09-14 22:25:19 -0400

    Fix comments and make Max happy.

--- questions/forms.py
+++ questions/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from questions.models import *
 
 class CommentForm(forms.Form):
-	comment = forms.CharField(max_length = 500, min_length = 5)
+	comment = forms.CharField(max_length = 500)
 
 	class Meta:
 		model = Comment
