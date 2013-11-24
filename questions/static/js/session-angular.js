e1f8afa7ad2dc8f300f9b7bc4aaa148ebeeda3de
Eric Pauley <epauley@outlook.com>
2013-11-24 00:56:58 -0500

    Hook up faceboo button.

--- questions/static/js/session-angular.js
+++ questions/static/js/session-angular.js
@@ -1,26 +1,31 @@
 var sessionApp = angular.module('sessionApp', ['ngRoute', 'ngSanitize', 'homepage', 'ngCookies']);
 
 sessionApp.controller('ProfileCtrl', function ProfileCtrl($scope, $http, $timeout, $rootScope, $sce) {
-	$http.get("/api/v1/user/"+GLOBALS['user']+"/", {
-		params: {full_pages: true}
+	$http.get("/api/v1/user/" + GLOBALS['user'] + "/", {
+		params: {
+			full_pages: true
+		}
 	}).success(function(data) {
 		$scope.user = data;
 	});
-	$scope.qfilter={"action_type":"question"}
-	$scope.tab="questions"
+	$scope.qfilter = {
+		"action_type": "question"
+	}
+	$scope.tab = "questions"
 });
 
-sessionApp.controller('SessionCtrl', function SessionCtrl($scope, $http, $timeout, $rootScope, $sce, $cookieStore){
+sessionApp.controller('SessionCtrl', function SessionCtrl($scope, $http, $timeout, $rootScope, $sce, $cookieStore) {
 	$scope.toApply = null;
-	function repeat(){
-		if(GLOBALS['owner'])
+
+	function repeat() {
+		if (GLOBALS['owner'])
 			$timeout(repeat, 10000);
 		else
 			$timeout(repeat, 30000);
-		$http.get("/api/v1/session/"+GLOBALS['session']+"/").success(function(data) {
-			if($scope.state.edit == null && $scope.refresh && $scope.state.questionEdit == 0){
+		$http.get("/api/v1/session/" + GLOBALS['session'] + "/").success(function(data) {
+			if ($scope.state.edit == null && $scope.refresh && $scope.state.questionEdit == 0) {
 				$scope.session = data;
-			}else{
+			} else {
 				$scope.toApply = data;
 			}
 		});
@@ -36,111 +41,140 @@ sessionApp.controller('SessionCtrl', function SessionCtrl($scope, $http, $timeou
 	$scope.state.question = ""
 	$scope.state.drafts = {}
 	$scope.refresh = true;
-	$scope.state.question = $cookieStore.get("askDrafts."+GLOBALS['session']) || "";
-	$cookieStore.remove("askDrafts."+GLOBALS['session']);
+	$scope.state.question = $cookieStore.get("askDrafts." + GLOBALS['session']) || "";
+	$cookieStore.remove("askDrafts." + GLOBALS['session']);
 
-	function reapply(){
+	function reapply() {
 		$timeout(reapply, 1000);
 	}
 	reapply();
 
-	$scope.$watch("state.edit", function(){
-		if($scope.toApply != null){
+	$scope.$watch("state.edit", function() {
+		if ($scope.toApply != null) {
 			$scope.session = $scope.toApply;
 			$scope.toApply = null;
 		}
-		if($scope.state.edit == "title"){
+		if ($scope.state.edit == "title") {
 			$("#edit-title").focus();
 		}
 	})
 
-	$scope.editQuestion = function(question){
+	$scope.facebook = function() {
+		FB.ui({
+				method: 'feed',
+				name: $scope.session.title,
+				link: document.location.href,
+				picture: $scope.session.image,
+				caption: $scope.session.name,
+				description: $scope.session.desc
+			},
+			function(response) {
+				
+			}
+		);
+	}
+
+	$scope.editQuestion = function(question) {
 		$http({
 			method: "PATCH",
 			url: question.resource_uri,
-			data: {question: question.question}
-		}).success(function(){
+			data: {
+				question: question.question
+			}
+		}).success(function() {
 
-		}).error(function(){
+		}).error(function() {
 
 		});
 	}
 
-	$scope.vote = function(question, val){
+	$scope.vote = function(question, val) {
 		var old = question.vote;
-		if(question.vote == val){
+		if (question.vote == val) {
 			question.vote = 0;
-		}else{
+		} else {
 			question.vote = val;
 		}
 		$http({
 			method: 'POST',
-			url: question.resource_uri+"vote/",
-			data: $.param({vote:question.vote}),
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			url: question.resource_uri + "vote/",
+			data: $.param({
+				vote: question.vote
+			}),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
 		});
 		question.score += question.vote - old;
 	}
 
-	$scope.delete = function(question, index){
+	$scope.delete = function(question, index) {
 		$http({
 			method: 'DELETE',
 			url: question.resource_uri,
-		}).success(function(){
+		}).success(function() {
 			$scope.session.questions.splice(index, 1);
 		});
 	}
 
-	$scope.star = function(question, val){
+	$scope.star = function(question, val) {
 		question.starred = val;
-		if(val){
+		if (val) {
 			var data = 1;
-		}else{
+		} else {
 			var data = 0;
 		}
 		$http({
 			method: 'POST',
-			url: question.resource_uri+"star/",
-			data: $.param({star:data}),
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			url: question.resource_uri + "star/",
+			data: $.param({
+				star: data
+			}),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
 		});
 	}
 
-	$scope.ask = function(){
+	$scope.ask = function() {
 		$http({
 			method: 'POST',
-			url: $scope.session.resource_uri+"ask/",
-			data: $.param({question:$scope.state.question}),
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			url: $scope.session.resource_uri + "ask/",
+			data: $.param({
+				question: $scope.state.question
+			}),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
 		}).
 		success(function(data, status, headers, config) {
 			$scope.session.questions.unshift(data.question);
 			$scope.state.question = "";
 			$scope.state.askTimer = moment().add('minutes', 1).valueOf();
-			$timeout(function(){
+			$timeout(function() {
 				$scope.state['askTimer'] = null;
 			}, $scope.state.askTimer - moment().unix());
 		}).
 		error(function(data, status, headers, config) {
 			$scope.state.askTimer = moment().add('minutes', 1).valueOf();
-			$timeout(function(){
+			$timeout(function() {
 				$scope.state['askTimer'] = null;
 			}, $scope.state.askTimer - moment().unix());
 		});
 	}
 
-	$scope.doAsk = function(){
-		if(GLOBALS['user']){
+	$scope.doAsk = function() {
+		if (GLOBALS['user']) {
 			$scope.ask();
-		}else{
-			$cookieStore.put("askDrafts."+GLOBALS['session'], $scope.state.question);
+		} else {
+			$cookieStore.put("askDrafts." + GLOBALS['session'], $scope.state.question);
 			$("#btnc").click();
 		}
 	}
 
-	$scope.edit = function(field, value){
+	$scope.edit = function(field, value) {
 		$scope.state.edit = null;
-		if($scope.toApply != null)
+		if ($scope.toApply != null)
 			$scope.toApply[field] = value;
 		data = {}
 		data[field] = value;
@@ -151,7 +185,7 @@ sessionApp.controller('SessionCtrl', function SessionCtrl($scope, $http, $timeou
 		})
 	}
 
-	$scope.answer = function(question, value){
+	$scope.answer = function(question, value) {
 		$scope.state.edit = null;
 		$scope.toApply = null;
 		$scope.state.answerdraft = false;
@@ -163,52 +197,60 @@ sessionApp.controller('SessionCtrl', function SessionCtrl($scope, $http, $timeou
 		question.answered = (value != "")
 		$http({
 			method: 'POST',
-			url: question.resource_uri+"answer/",
-			data: $.param({answer: value}),
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			url: question.resource_uri + "answer/",
+			data: $.param({
+				answer: value
+			}),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
 		})
 	}
 
-	$rootScope.$on("tabChange", function(event, args){
+	$rootScope.$on("tabChange", function(event, args) {
 		angular.extend($scope, args)
 		$scope.state.answering = false
 	})
 
-	$scope.endSession = function(){
+	$scope.endSession = function() {
 		$http({
 			method: "PATCH",
-			url: "/api/v1/session/"+GLOBALS['session']+"/",
-			data: {end_time: Date()}
+			url: "/api/v1/session/" + GLOBALS['session'] + "/",
+			data: {
+				end_time: Date()
+			}
 		})
 	}
 
-	changeImage = function(){
+	changeImage = function() {
 		$("#image-edit").ajaxSubmit({
-			success:function(data){
-	            $scope.session.image = data.thumbnail;
-	            $scope.$apply();
-	        }
+			success: function(data) {
+				$scope.session.image = data.thumbnail;
+				$scope.$apply();
+			}
 		})
 	}
 });
 
 sessionApp.directive('focusOnShow', function($timeout) {
-    return {
-        link: function ( scope, element, attrs ) {
-            scope.$watch( attrs.ngShow, function ( val ) {
-                if ( angular.isDefined( val ) && val ) {
-                    $timeout( function () { element[0].focus(); } );
-                }
-            }, true);
+	return {
+		link: function(scope, element, attrs) {
+			scope.$watch(attrs.ngShow, function(val) {
+				if (angular.isDefined(val) && val) {
+					$timeout(function() {
+						element[0].focus();
+					});
+				}
+			}, true);
 
-            element.bind('blur', function () {
-                if ( angular.isDefined( attrs.ngFocusLost ) ) {
-                    scope.$apply( attrs.ngFocusLost );
+			element.bind('blur', function() {
+				if (angular.isDefined(attrs.ngFocusLost)) {
+					scope.$apply(attrs.ngFocusLost);
 
-                }
-            });
-        }
-    };
+				}
+			});
+		}
+	};
 });
 
 sessionApp.filter('plain', function() {
@@ -217,16 +259,18 @@ sessionApp.filter('plain', function() {
 	}
 });
 
-sessionApp.filter('linesplit', ['$sce', function($sce){
-	return function(input){
-		input = $.trim(input)
-		input = "<p>"+input.replace(/(\&\#10;)/g, "<br/>")+"</p>"
-		return $sce.trustAsHtml(input);
-	};
-}]);
+sessionApp.filter('linesplit', ['$sce',
+	function($sce) {
+		return function(input) {
+			input = $.trim(input)
+			input = "<p>" + input.replace(/(\&\#10;)/g, "<br/>") + "</p>"
+			return $sce.trustAsHtml(input);
+		};
+	}
+]);
 
-sessionApp.filter('countdown', function(){
-	return function(input, prefix){
+sessionApp.filter('countdown', function() {
+	return function(input, prefix) {
 		return moment(input).fromNow(prefix);
 	};
 });
