ebb71a796b80cf905ca7e3cc2d16e8256327ff57
Eric Pauley <epauley@outlook.com>
2014-01-09 23:42:56 -0500

    Don't rate-limit comments for owner.

--- questions/static/js/session-routing.js
+++ questions/static/js/session-routing.js
@@ -1,73 +1,94 @@
-sessionApp.controller("UnansweredCtrl", function UnansweredCtrl($scope, $rootScope){
-	$rootScope.$broadcast("tabChange", {"qfilter":{'answered':'false'}, "tab":"unanswered"})
-	$scope.state = $rootScope
+sessionApp.controller("UnansweredCtrl", function UnansweredCtrl($scope, $rootScope) {
+    $rootScope.$broadcast("tabChange", {
+        "qfilter": {
+            'answered': 'false'
+        },
+        "tab": "unanswered"
+    })
+    $scope.state = $rootScope
 })
 
-sessionApp.controller("AnsweredCtrl", function AnsweredCtrl($scope, $rootScope){
-	$rootScope.$broadcast("tabChange", {"qfilter":{'answered':'true'}, "tab":"answered"})
-	$scope.state = $rootScope
+sessionApp.controller("AnsweredCtrl", function AnsweredCtrl($scope, $rootScope) {
+    $rootScope.$broadcast("tabChange", {
+        "qfilter": {
+            'answered': 'true'
+        },
+        "tab": "answered"
+    })
+    $scope.state = $rootScope
 })
 
-sessionApp.controller("StarredCtrl", function StarredCtrl($scope, $rootScope){
-	$rootScope.$broadcast("tabChange", {"qfilter":{'starred':'true'}, "tab":"starred"})
-	$scope.state = $rootScope
+sessionApp.controller("StarredCtrl", function StarredCtrl($scope, $rootScope) {
+    $rootScope.$broadcast("tabChange", {
+        "qfilter": {
+            'starred': 'true'
+        },
+        "tab": "starred"
+    })
+    $scope.state = $rootScope
 })
 
-sessionApp.controller("QuestionCtrl", function QuestionCtrl($scope, $rootScope, $routeParams, $http){
-	$rootScope.$broadcast("tabChange", {
-		"qfilter":{'id':$routeParams.questionId.toString()},
-		"tab":"question"
-	})
-	$scope.state = $rootScope;
-	$scope.state.comment = "";
-	$http.get("/api/v1/comment/", {
-		params: {question:$routeParams.questionId}
-	}).success(function(data){
-		$scope.comments = data.objects;
-	});
-	$scope.postComment = function(){
-		$http.post("/api/v1/comment/", {
-			question:"/api/v1/question/"+$routeParams.questionId+"/",
-			comment: $scope.state.comment
-		}).success(function(data, status, headers){
-			$scope.state.comment = "";
-			$http.get(headers("Location")).success(function(data){
-				$scope.comments.unshift(data);
-			});
-			$scope.state.commentTimer = moment().add('minutes', 1).valueOf();
-			$timeout(function(){
-				$scope.state['commentTimer'] = null;
-			}, $scope.state.commentTimer - moment().unix());
-			console.log($scope.state.commentTimer - moment().valueOf())
-		}).error(function(data){
-			if(data.reason == 'too_soon'){
-				$scope.state.commentTimer = Date.parse(data.soonest);
-				$timeout(function(){
-					$scope.state['commentTimer'] = null;
-				}, $scope.state.commentTimer - moment().valueOf());
-			}
-		})
-	}
+sessionApp.controller("QuestionCtrl", function QuestionCtrl($scope, $rootScope, $routeParams, $http) {
+    $rootScope.$broadcast("tabChange", {
+        "qfilter": {
+            'id': $routeParams.questionId.toString()
+        },
+        "tab": "question"
+    })
+    $scope.state = $rootScope;
+    $scope.state.comment = "";
+    $http.get("/api/v1/comment/", {
+        params: {
+            question: $routeParams.questionId
+        }
+    }).success(function(data) {
+        $scope.comments = data.objects;
+    });
+    $scope.postComment = function() {
+        $http.post("/api/v1/comment/", {
+            question: "/api/v1/question/" + $routeParams.questionId + "/",
+            comment: $scope.state.comment
+        }).success(function(data, status, headers) {
+            $scope.state.comment = "";
+            $http.get(headers("Location")).success(function(data) {
+                $scope.comments.unshift(data);
+            });
+            if (!$scope.state.owner) {
+                $scope.state.commentTimer = moment().add('minutes', 1).valueOf();
+                $timeout(function() {
+                    $scope.state['commentTimer'] = null;
+                }, $scope.state.commentTimer - moment().unix());
+            }
+        }).error(function(data) {
+            if (data.reason == 'too_soon') {
+                $scope.state.commentTimer = Date.parse(data.soonest);
+                $timeout(function() {
+                    $scope.state['commentTimer'] = null;
+                }, $scope.state.commentTimer - moment().valueOf());
+            }
+        })
+    }
 })
 
 sessionApp.config(function($routeProvider, $locationProvider, $sceDelegateProvider) {
-	$sceDelegateProvider.resourceUrlWhitelist([
-    'self',
-    'http://static.ama.io/**']);
-	$routeProvider.when('/s/:sessionId/unanswered', {
-		controller: 'UnansweredCtrl',
-		templateUrl: GLOBALS['session_html']
-	}).when('/s/:sessionId/answered', {
-		controller: 'AnsweredCtrl',
-		templateUrl: GLOBALS['session_html']
-	}).when('/s/:sessionId/starred', {
-		controller: 'StarredCtrl',
-		templateUrl: GLOBALS['session_html']
-	}).when('/q/:questionId', {
-		controller: 'QuestionCtrl',
-		templateUrl: GLOBALS['session_html']
-	}).otherwise({
-		redirectTo: '/s/'+GLOBALS['session']+'/unanswered'
-	});
-	$locationProvider.html5Mode(true);
- });
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'http://static.ama.io/**'
+    ]);
+    $routeProvider.when('/s/:sessionId/unanswered', {
+        controller: 'UnansweredCtrl',
+        templateUrl: GLOBALS['session_html']
+    }).when('/s/:sessionId/answered', {
+        controller: 'AnsweredCtrl',
+        templateUrl: GLOBALS['session_html']
+    }).when('/s/:sessionId/starred', {
+        controller: 'StarredCtrl',
+        templateUrl: GLOBALS['session_html']
+    }).when('/q/:questionId', {
+        controller: 'QuestionCtrl',
+        templateUrl: GLOBALS['session_html']
+    }).otherwise({
+        redirectTo: '/s/' + GLOBALS['session'] + '/unanswered'
+    });
+    $locationProvider.html5Mode(true);
+});
