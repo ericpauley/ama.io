ce071b355b2f9b5abb4423b91a99b278f44c15e5
Eric Pauley <epauley@outlook.com>
2014-02-16 00:06:33 -0500

    Exactly.

--- ama/settings.py
+++ ama/settings.py
@@ -198,7 +198,7 @@ AUTHENTICATION_BACKENDS = (
     # Default backend
     'django.contrib.auth.backends.ModelBackend',
     # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
+    # "allauth.account.auth_backends.AuthenticationBackend",
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
