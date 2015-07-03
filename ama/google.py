0b18bc320cf1498ec777a6944a3dcf7d0db3f65e
Eric Pauley <eric@pauley.me>
2015-07-03 01:59:17 -0400

    Fix google integration.

--- ama/google.py
+++ ama/google.py
@@ -2,11 +2,19 @@ from oauth2client.client import SignedJwtAssertionCredentials
 import httplib2
 from django.conf import settings
 from apiclient import discovery
+import os
+import json
+try:
+    data = json.load(open(os.path.join(settings.PROJECT_PATH, 'google.json')))
+except:
+    data = None
 
-#credentials = SignedJwtAssertionCredentials.from_json(settings.GOOGLE_KEY)
-#http = httplib2.Http()
-#credentials.refresh(http)
-#http = credentials.authorize(http)
+if data is not None:
+    credentials = SignedJwtAssertionCredentials(data['client_email'], data['private_key'], "https://www.googleapis.com/auth/analytics.readonly")
+    http = httplib2.Http()
+    credentials.refresh(http)
+    http = credentials.authorize(http)
 
-#service = discovery.build('analytics', 'v3', http=http)
-service = {}
+    service = discovery.build('analytics', 'v3', http=http)
+else:
+    service = {}
